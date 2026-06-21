6608f25b584515b196c9856cc8ea8f9fe7a7315c
JoseGarciaMayen <josegarciamayen@gmail.com>
2026-06-21 17:19:15 +0200

    refactor: clean up train_cnn_mlp

--- src/training/train_cnn_mlp.py
+++ src/training/train_cnn_mlp.py
@@ -17,6 +17,7 @@ from tensorflow.keras.layers import (  # type: ignore
 )
 from tensorflow.keras.models import Model  # type: ignore
 
+from src.config import DATA
 from src.training.trainer import TrainerBase
 from src.utils import notify_telegram
 
@@ -36,164 +37,92 @@ class TrainerCNNMLP(TrainerBase):
             "units_mlp2": int,
             "units_mlp3": int,
         }
+        return {k: param_types.get(k, str)(v) for k, v in best_params.items()}
 
-        typed_best_params = {}
-        for k, v in best_params.items():
-            type = param_types.get(k, str)
-            typed_best_params[k] = type(v)
-        return typed_best_params
+    def _split_inputs(self, X):
+        return [X.iloc[:, :187].values.reshape(-1, 187, 1), X.iloc[:, 187:].values]
 
     def create_model(self, input_shape_cnn=(187, 1), input_shape_mlp=(36,), num_classes=4):
-        """
-        Function to create the CNNMLP model with dynamic hyperparameters.
-        """
-        best_params = self.get_typed_params(self.get_params())
+        p = self.get_typed_params(self.get_params())
 
-        mlflow.log_param("input_shape_cnn", input_shape_cnn)
-        mlflow.log_param("input_shape_mlp", input_shape_mlp)
-        mlflow.log_param("num_classes", num_classes)
-        mlflow.log_param("l2", best_params["l2"])
-        mlflow.log_param("dropout", best_params["dropout"])
-        mlflow.log_param("learning_rate", best_params["learning_rate"])
-        mlflow.log_param("filters", best_params["filters"])
-        mlflow.log_param("filter_multiplier", best_params["filter_multiplier"])
-        mlflow.log_param("units_mlp1", best_params["units_mlp1"])
-        mlflow.log_param("units_mlp2", best_params["units_mlp2"])
-        mlflow.log_param("units_mlp3", best_params["units_mlp3"])
-        mlflow.log_param("conv_layers", 4)
+        mlflow.log_params(
+            {
+                "input_shape_cnn": input_shape_cnn,
+                "input_shape_mlp": input_shape_mlp,
+                "num_classes": num_classes,
+                "conv_layers": 4,
+                **p,
+            }
+        )
 
-        input_cnn = Input(input_shape_cnn)
-        x = Conv1D(
-            filters=best_params["filters"],
-            kernel_size=5,
-            activation="relu",
-            kernel_regularizer=regularizers.l2(best_params["l2"]),
-            padding="same",
-        )(input_cnn)
-        x = BatchNormalization()(x)
-        x = MaxPooling1D(pool_size=2)(x)
-        x = Dropout(best_params["dropout"])(x)
-        x = Conv1D(
-            filters=best_params["filters"] * best_params["filter_multiplier"],
-            kernel_size=5,
-            activation="relu",
-            kernel_regularizer=regularizers.l2(best_params["l2"]),
-            padding="same",
-        )(x)
-        x = BatchNormalization()(x)
-        x = MaxPooling1D(pool_size=2)(x)
-        x = Dropout(best_params["dropout"])(x)
-        x = Conv1D(
-            filters=best_params["filters"]
-            * best_params["filter_multiplier"]
-            * best_params["filter_multiplier"],
-            kernel_size=5,
-            activation="relu",
-            kernel_regularizer=regularizers.l2(best_params["l2"]),
-            padding="same",
-        )(x)
-        x = BatchNormalization()(x)
-        x = MaxPooling1D(pool_size=2)(x)
-        x = Dropout(best_params["dropout"])(x)
-        x = Conv1D(
-            filters=best_params["filters"]
-            * best_params["filter_multiplier"]
-            * best_params["filter_multiplier"],
-            kernel_size=5,
-            activation="relu",
-            kernel_regularizer=regularizers.l2(best_params["l2"]),
-            padding="same",
-        )(x)
-        x = BatchNormalization()(x)
-        x = MaxPooling1D(pool_size=2)(x)
-        x = Dropout(best_params["dropout"])(x)
-
+        conv_filters = [
+            p["filters"],
+            p["filters"] * p["filter_multiplier"],
+            p["filters"] * p["filter_multiplier"] ** 2,
+            p["filters"] * p["filter_multiplier"] ** 2,
+        ]
+        x = Input(input_shape_cnn)
+        input_cnn = x
+        for f in conv_filters:
+            x = Conv1D(
+                f, 5, activation="relu", kernel_regularizer=regularizers.l2(p["l2"]), padding="same"
+            )(x)
+            x = BatchNormalization()(x)
+            x = MaxPooling1D(2)(x)
+            x = Dropout(p["dropout"])(x)
         x = Flatten()(x)
+
         input_mlp = Input(input_shape_mlp)
-        y = Dense(
-            best_params["units_mlp1"],
-            activation="relu",
-            kernel_regularizer=regularizers.l2(best_params["l2"]),
-        )(input_mlp)
-        y = Dense(
-            best_params["units_mlp2"],
-            activation="relu",
-            kernel_regularizer=regularizers.l2(best_params["l2"]),
-        )(y)
-        y = Dense(
-            best_params["units_mlp3"],
-            activation="relu",
-            kernel_regularizer=regularizers.l2(best_params["l2"]),
-        )(y)
-        y = Dense(64, activation="relu", kernel_regularizer=regularizers.l2(best_params["l2"]))(y)
-        y = Dense(32, activation="relu", kernel_regularizer=regularizers.l2(best_params["l2"]))(y)
+        y = input_mlp
+        for units in [p["units_mlp1"], p["units_mlp2"], p["units_mlp3"], 64, 32]:
+            y = Dense(units, activation="relu", kernel_regularizer=regularizers.l2(p["l2"]))(y)
 
         combined = concatenate([x, y])
-
-        z = Dense(64, activation="relu", kernel_regularizer=regularizers.l2(best_params["l2"]))(
-            combined
-        )
-        z = Dropout(best_params["dropout"])(z)
-        z = Dense(32, activation="relu", kernel_regularizer=regularizers.l2(best_params["l2"]))(z)
-        z = Dropout(best_params["dropout"])(z)
-
+        z = Dense(64, activation="relu", kernel_regularizer=regularizers.l2(p["l2"]))(combined)
+        z = Dropout(p["dropout"])(z)
+        z = Dense(32, activation="relu", kernel_regularizer=regularizers.l2(p["l2"]))(z)
+        z = Dropout(p["dropout"])(z)
         output = Dense(num_classes, activation="linear")(z)
 
         model = Model(inputs=[input_cnn, input_mlp], outputs=output)
-
         model.compile(
             loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
-            optimizer=tf.keras.optimizers.Adam(best_params["learning_rate"]),
+            optimizer=tf.keras.optimizers.Adam(p["learning_rate"]),
             metrics=["accuracy"],
         )
-
         return model
 
     def save_model(self, model, model_name):
-        model.save("src/saved_models/" + model_name + ".keras")
+        model.save(f"src/saved_models/{model_name}.keras")
 
     def mlflow_start(self, model, X_train, y_train, X_cv, y_cv, class_weights):
-
-        mlflow.tensorflow.autolog(
-            log_models=False,
-            log_datasets=False,
-            disable=False,
-            exclusive=False,
-            disable_for_unsupported_versions=False,
-            silent=True,
-        )
+        mlflow.tensorflow.autolog(log_models=False, log_datasets=False, silent=True)
 
         with mlflow.start_run(nested=True):
             model.fit(
-                [X_train.iloc[:, :187].values.reshape(-1, 187, 1), X_train.iloc[:, 187:].values],
+                self._split_inputs(X_train),
                 y_train,
                 class_weight=class_weights,
-                validation_data=(
-                    [X_cv.iloc[:, :187].values.reshape(-1, 187, 1), X_cv.iloc[:, 187:].values],
-                    y_cv,
-                ),
+                validation_data=(self._split_inputs(X_cv), y_cv),
                 epochs=50,
             )
 
-            loss, acc = model.evaluate(
-                [X_train.iloc[:, :187].values.reshape(-1, 187, 1), X_train.iloc[:, 187:].values],
-                y_train,
-            )
-            mlflow.log_metric("accuracy", acc)
-            mlflow.log_metric("loss", loss)
-
-            val_loss, val_acc = model.evaluate(
-                [X_cv.iloc[:, :187].values.reshape(-1, 187, 1), X_cv.iloc[:, 187:].values], y_cv
-            )
-            mlflow.log_metric("val_accuracy", val_acc)
-            mlflow.log_metric("val_loss", val_loss)
-
-            y_cv_logits = model.predict(
-                [X_cv.iloc[:, :187].values.reshape(-1, 187, 1), X_cv.iloc[:, 187:].values]
-            )
-            y_cv_pred = np.argmax(y_cv_logits, axis=1)
+            loss, acc = model.evaluate(self._split_inputs(X_train), y_train, verbose=0)
+            val_loss, val_acc = model.evaluate(self._split_inputs(X_cv), y_cv, verbose=0)
+            y_cv_pred = np.argmax(model.predict(self._split_inputs(X_cv)), axis=1)
             val_f1 = f1_score(y_cv, y_cv_pred, average="macro")
-            mlflow.log_metric("val_f1_macro", val_f1)
+            val_f1_weighted = f1_score(y_cv, y_cv_pred, average="weighted")
+
+            mlflow.log_metrics(
+                {
+                    "accuracy": acc,
+                    "loss": loss,
+                    "val_accuracy": val_acc,
+                    "val_loss": val_loss,
+                    "val_f1_macro": val_f1,
+                    "val_f1_weighted": val_f1_weighted,
+                }
+            )
 
             mlflow.keras.log_model(model, self.model_name, registered_model_name=self.model_name)
             self.save_model(model, self.model_name)
@@ -203,19 +132,11 @@ class TrainerCNNMLP(TrainerBase):
             return val_f1
 
     def train(self):
-        """
-        Function to train the CNN+MLP model with the best hyperparameters found by Optuna.
-        It logs the model and metrics to MLflow.
-        """
         X_train, y_train, X_cv, y_cv, class_weights = self.load_data(
-            "data/processed/feat/mitbih_train_features.csv",
-            "data/processed/feat/mitbih_cv_features.csv",
+            DATA["feat_train"], DATA["feat_cv"]
         )
-
         model = self.create_model()
-
         val_f1 = self.mlflow_start(model, X_train, y_train, X_cv, y_cv, class_weights)
-
         return val_f1
 
 
